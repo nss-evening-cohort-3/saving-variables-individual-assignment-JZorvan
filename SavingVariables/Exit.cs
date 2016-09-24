4fbf950a97df2863ea5ef77e152a5f2959cdcb31
JZorvan <jzorvan@gmail.com>
2016-09-24 12:01:05 -0700

    Exit commands work

---
+++ SavingVariables/Exit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SavingVariables
+{
+    public class Exit
+    {
+        public void ExitProgram()
+        {
+            Thread.Sleep(1000);  // One second delay
+            Environment.Exit(0);  // Exits program
+        }
+    }
+}
--- SavingVariables/Program.cs
+++ SavingVariables/Program.cs
@@ -10,6 +10,47 @@ namespace SavingVariables
     {
         static void Main(string[] args)
         {
+            // Program Variables
+            string Prompt = "[{0}]>";
+            int Counter = 0;
+            bool GoForth = true;
+            string bye = "See you next time!";
+
+            // Instantiate Classes
+            Evaluate evaluate = new Evaluate();
+            Exit exit = new Exit();
+
+            // Print an introduction to the program
+            Console.WriteLine("***  Welcome to Saving Variables!  ***\r\n_______________________________________\r\n");
+
+            // Runs program loop
+            while (GoForth == true)
+            {
+                Console.Write(Prompt, Counter);  // Prints prompt
+                String UserInput = Console.ReadLine().ToLower();  // Collects input in lower case
+
+                switch (UserInput)
+                {
+                    case "exit":
+
+                        Console.WriteLine(bye);
+                        exit.ExitProgram();
+                        break;
+
+                    case "quit":
+
+                        Console.WriteLine(bye);
+                        exit.ExitProgram();
+                        break;
+
+                    default:
+                        break;
+                }
+
+                Counter++;  // Increases prompt counter
+            }
+
+            Console.Read();
         }
     }
 }
